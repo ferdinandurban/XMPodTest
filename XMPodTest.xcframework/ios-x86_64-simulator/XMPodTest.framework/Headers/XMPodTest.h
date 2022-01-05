fcfd29daf175879746d2be3b0a234211bc67ec07
Ferdinand Urban <ferdinand.urban@gmail.com>
2022-01-05 23:33:54 +0100

    init1

---
+++ XMPodTest.xcframework/ios-x86_64-simulator/XMPodTest.framework/Headers/XMPodTest.h
@@ -0,0 +1,18 @@
+//
+//  XMPodTest.h
+//  XMPodTest
+//
+//  Created by Ferdinand Urban on 05.01.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XMPodTest.
+FOUNDATION_EXPORT double XMPodTestVersionNumber;
+
+//! Project version string for XMPodTest.
+FOUNDATION_EXPORT const unsigned char XMPodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMPodTest/PublicHeader.h>
+
+
